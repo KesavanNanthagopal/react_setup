2c6ba91e5da1ae5ec0e8e282d50359c84da2be57
kesavan.N <kesavannanthagopal2000@gmail.com>
2024-12-20 18:58:14 +0530

    changed the menu and added conformation popup for delete

--- my-react-app/src/container/Menu.tsx
+++ my-react-app/src/container/Menu.tsx
@@ -1,9 +1,31 @@
 import { useState } from 'react';
 import biryani from '../assets/images/biryani.jpg'
 import AddMenuModal from '../component/AddMenu';
+import { menuItems } from '../utils/const';
+import ConformationPopup from "../component/ConformationPopup";
 
 function Menu() {
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [showPopup, setShowPopup] = useState(false);
+    const [menuId, setMenuId] = useState(null);
+
+    const handleEdit = (id: any) => {
+        setMenuId(id)
+        setIsModalVisible(true)
+    };
+
+    const handleDelete = (id: any) => {
+        setMenuId(id)
+        setShowPopup(true)
+    };
+
+    const handleConformDelete = () => {
+        setShowPopup(false)
+    };
+
+    const handleCancel = () => {
+        setShowPopup(false)
+    };
 
     const toggleModal = () => {
         setIsModalVisible(!isModalVisible);
@@ -34,97 +56,44 @@ function Menu() {
             <div className="container mt-4 menu">
                 <h3 className="menuheader">Menu</h3>
                 <div className="row menu-row" style={{ maxHeight: "40vh", overflowY: "auto" }}>
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$12.99</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
-                            </div>
-                        </div>
-                    </div>
+                    {menuItems.map((item) => (
+                        <div className="col-md-3" key={item.id}>
+                            <div className="card position-relative">
+                                <div className="position-absolute top-0 end-0 p-2">
+                                    <i
+                                        className="bi bi-pencil-square mx-2"
+                                        onClick={() => handleEdit(item.id)}
+                                        title="Edit"
+                                        style={{ color: "orange", cursor: "pointer" }}
+                                    ></i>
+                                    <i
+                                        className="bi bi-trash mx-2"
+                                        onClick={() => handleDelete(item.id)}
+                                        title="Delete"
+                                        style={{ color: "gray", cursor: "pointer" }}
+                                    ></i>
+                                </div>
 
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
+                                <img src={biryani} className="card-img-top" alt={item.name} />
+                                <div className="card-body">
+                                    <h5 className="card-title menuName">{item.name}</h5>
+                                    <p className="card-text menuPrice">{item.price}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-3">
-                        <div className="card">
-                            <img src={biryani} className="card-img-top" alt="Menu item" />
-                            <div className="card-body">
-                                <h5 className="card-title menuName">Menu Item</h5>
-                                <p className="card-text menuPrice">$15.99</p>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
             <AddMenuModal isVisible={isModalVisible} onClose={toggleModal} />
+            {showPopup && (
+                <ConformationPopup
+                    message="Do you want to delete this menu?"
+                    confirmButtonLabel="Yes"
+                    cancelButtonLabel="No"
+                    onConfirm={handleConformDelete}
+                    onCancel={handleCancel}
+                />
+            )}
         </div>
     );
 }
---
+++ my-react-app/src/utils/const.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+    { id: 1, name: "Chicken Biryani", price: "$12.99", image: "biryani.jpg" },
+    { id: 2, name: "Paneer Butter Masala", price: "$10.99", image: "paneer.jpg" },
+    { id: 3, name: "Tandoori Chicken", price: "$15.99", image: "tandoori.jpg" },
+    { id: 4, name: "Mutton Curry", price: "$18.99", image: "mutton.jpg" },
+    { id: 5, name: "Veg Fried Rice", price: "$9.99", image: "friedrice.jpg" },
+    { id: 6, name: "Butter Naan", price: "$2.99", image: "naan.jpg" },
+];
