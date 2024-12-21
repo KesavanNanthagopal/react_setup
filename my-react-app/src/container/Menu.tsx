7bd18c7c65cd4d504da228de0b779e38172e196c
kesavan.N <kesavannanthagopal2000@gmail.com>
2024-12-21 17:58:48 +0530

    menu design changes

--- my-react-app/src/container/Menu.tsx
+++ my-react-app/src/container/Menu.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import biryani from '../assets/images/biryani.jpg'
+import biryani from '../assets/images/biryani.jpg';
 import AddMenuModal from '../component/AddMenu';
 import { menuItems } from '../utils/const';
 import ConformationPopup from "../component/ConformationPopup";
@@ -9,33 +9,38 @@ function Menu() {
     const [showPopup, setShowPopup] = useState(false);
     const [menuId, setMenuId] = useState(null);
 
-    const handleEdit = (id: any) => {
-        setMenuId(id)
-        setIsModalVisible(true)
+    const handleEdit = (id:any) => {
+        setMenuId(id);
+        setIsModalVisible(true);
     };
 
-    const handleDelete = (id: any) => {
-        setMenuId(id)
-        setShowPopup(true)
+    const handleDelete = (id:any) => {
+        setMenuId(id);
+        setShowPopup(true);
     };
 
     const handleConformDelete = () => {
-        setShowPopup(false)
+        setShowPopup(false);
     };
 
     const handleCancel = () => {
-        setShowPopup(false)
+        setShowPopup(false);
     };
 
     const toggleModal = () => {
         setIsModalVisible(!isModalVisible);
     };
+
     return (
         <div className="container mt-4 menu">
             <div className="row mb-4">
-                <div className="col-md-6">
+                <div className="col-md-6 col-12 mb-2 mb-md-0">
                     <div className="input-group">
-                        <span className="input-group-text" id="basic-addon1" style={{ backgroundColor: "#FC8019", color: "#fff" }}>
+                        <span
+                            className="input-group-text"
+                            id="basic-addon1"
+                            style={{ backgroundColor: "#FC8019", color: "#fff" }}
+                        >
                             <i className="bi bi-search"></i>
                         </span>
                         <input
@@ -47,42 +52,39 @@ function Menu() {
                         />
                     </div>
                 </div>
-                <div className="col-md-6 text-end">
-                    <button className="btn" onClick={toggleModal}>
+                <div className="col-md-6 col-12 text-md-end text-center">
+                    <button className="btn btn-primary" onClick={toggleModal}>
                         Add Menu
                     </button>
                 </div>
             </div>
-            <div className="container mt-4 menu">
-                <h3 className="menuheader">Menu</h3>
-                <div className="row menu-row" style={{ maxHeight: "40vh", overflowY: "auto" }}>
-                    {menuItems.map((item) => (
-                        <div className="col-md-3" key={item.id}>
-                            <div className="card position-relative">
-                                <div className="position-absolute top-0 end-0 p-2">
-                                    <i
-                                        className="bi bi-pencil-square mx-2"
-                                        onClick={() => handleEdit(item.id)}
-                                        title="Edit"
-                                        style={{ color: "orange", cursor: "pointer" }}
-                                    ></i>
-                                    <i
-                                        className="bi bi-trash mx-2"
-                                        onClick={() => handleDelete(item.id)}
-                                        title="Delete"
-                                        style={{ color: "gray", cursor: "pointer" }}
-                                    ></i>
-                                </div>
-
-                                <img src={biryani} className="card-img-top" alt={item.name} />
-                                <div className="card-body">
-                                    <h5 className="card-title menuName">{item.name}</h5>
-                                    <p className="card-text menuPrice">{item.price}</p>
-                                </div>
+            <h3 className="menuheader">Menu</h3>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+                {menuItems.map((item) => (
+                    <div className="col" key={item.id}>
+                        <div className="card h-100 position-relative">
+                            <div className="position-absolute top-0 end-0 p-2">
+                                <i
+                                    className="bi bi-pencil-square mx-2"
+                                    onClick={() => handleEdit(item.id)}
+                                    title="Edit"
+                                    style={{ color: "orange", cursor: "pointer" }}
+                                ></i>
+                                <i
+                                    className="bi bi-trash mx-2"
+                                    onClick={() => handleDelete(item.id)}
+                                    title="Delete"
+                                    style={{ color: "gray", cursor: "pointer" }}
+                                ></i>
+                            </div>
+                            <img src={biryani} className="card-img-top img-fluid" alt={item.name} />
+                            <div className="card-body text-center">
+                                <h5 className="card-title menuName">{item.name}</h5>
+                                <p className="card-text menuPrice">{item.price}</p>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
             <AddMenuModal isVisible={isModalVisible} onClose={toggleModal} />
             {showPopup && (
